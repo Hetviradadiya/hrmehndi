003b4d126ee2a10f697968bf94944c97558b9b64
Hetviradadiya <radadiyahetvi85@gmail.com>
2026-07-27 17:28:08 +0530

    fix

--- hrmehndi_project/urls.py
+++ hrmehndi_project/urls.py
@@ -8,3 +8,5 @@ urlpatterns = [
     path('accounts/', include('django.contrib.auth.urls')),
     path('', include('bookings.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
